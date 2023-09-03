8916d9b5c23f3d96ae225b2aa099478d2b59b98f
marcus <mc@marcuscatt.co.uk>
2023-09-03 12:17:03 +0530

    new file

---
+++ src/test/java/com/infuse/it/steps/DefinitionSteps.java
@@ -0,0 +1,34 @@
+package com.infuse.it.steps;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+//import cucumber.api.java.en.Given;
+//import cucumber.api.java.en.Then;
+//import cucumber.api.java.en.When;
+
+import com.infuse.it.steps.serenity.EndUserSteps;
+
+public class DefinitionSteps {
+
+    @Steps
+    EndUserSteps anna;
+
+    @Given("the user is on the Wikionary home page")
+    public void givenTheUserIsOnTheWikionaryHomePage() {
+        anna.is_the_home_page();
+    }
+
+    @When("the user looks up the definition of the word '(.*)'")
+    public void whenTheUserLooksUpTheDefinitionOf(String word) {
+        anna.looks_for(word);
+    }
+
+    @Then("they should see the definition '(.*)'")
+    public void thenTheyShouldSeeADefinitionContainingTheWords(String definition) {
+        anna.should_see_definition(definition);
+    }
+
+}
